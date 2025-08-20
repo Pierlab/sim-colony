aa6e2d23c74c5b8fab83cf7e6c9a27315005a0e8
Pierlab <plab71@hotmail.com>
2025-08-20 22:17:20 +0200

    feat(city): add city scene with influence and road link

--- scripts/city.gd
+++ scripts/city.gd
@@ -1,7 +1,17 @@
-extends Node2D
+extends Area2D
+
+signal road_connected(road: Node)
 
 @export var influence_radius: float = 250.0
+var occupied: bool = false
+var connected_road: Node = null
 
-func _ready() -> void:
+func occupy() -> void:
+    if occupied:
+        return
     Influence.add_zone(global_position, influence_radius)
+    occupied = true
 
+func connect_road(road: Node) -> void:
+    connected_road = road
+    road_connected.emit(road)
